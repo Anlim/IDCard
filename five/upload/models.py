af3d7ba1a4f215535a159ce24a14a6d1f83ea80a
Five-Zero <zhengxiongzuo@gmail.com>
2017-08-22 18:00:32 +0800

    Come to an end

--- five/upload/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
-
-class IMG(models.Model):
-    img = models.ImageField(upload_to='material') #图片存储位置
